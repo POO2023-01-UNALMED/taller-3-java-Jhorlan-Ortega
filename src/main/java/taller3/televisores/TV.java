fa379a3fc169ac09c5e60a9b810e65d97f7997d9
Jhorlan-Ortega <124930448+Jhorlan-Ortega@users.noreply.github.com>
2023-02-28 00:45:52 -0500

    Intento_11

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -3,19 +3,17 @@ package taller3.televisores;
 
 public class TV {
     private Marca marca;
-    private int canal;
-    private int precio;
+    private int canal=1;
+    private int precio=500;
     private boolean estado;
-    private int volumen;
+    private int volumen=1;
     Control control;
     private static int numTV;
      
     public TV (Marca marca, boolean estado){
         this.marca=marca;
         this.estado=estado;
-        this.canal=1;
-        this.precio=500;
-        this.volumen=1;  
+          
         this.numTV++;
     }
     
@@ -49,8 +47,8 @@ public class TV {
         return volumen;
     }
     
-    public void setVolumen( int newVolumen){
-        this.volumen=newVolumen;
+    public void setVolumen( int volumen){
+        this.volumen=volumen;
     }
    
     public int getCanal(){
@@ -82,35 +80,28 @@ public class TV {
         return estado;
     }
 
-    public void canalUp(){
-        if (estado==true){
-            if (1<=canal && canal<120){
-                ++canal;   
-            } 
-        }  
+    public void canalUp () {
+	if ((this.estado == true) && (this.canal < 120) ) {
+		this.canal++;
+	}
     }
-    public void canalDown(){
-        if (estado==true){
-            if (1<canal && canal<=120){
-                 --canal; 
-            }
-        }
+	
+    public void canalDown () {
+	if ((this.estado == true) && (this.canal > 1) ) {
+		this.canal--;
+	}
     }
-    
-    public void volumenUp(){
-        if (estado==true){
-            if (1<=volumen && volumen<7){ 
-                 ++volumen;
-            }
-        }   
-    }    
-    
-    public void volumenDown(){
-        if (estado==true){
-            if (1<volumen && volumen<=7){ 
-                --volumen;       
-            }
-        }
+	
+    public void volumenUp () {
+	if ((this.estado == true) && (this.volumen < 7) ) {
+		this.volumen++;
+	}
+    }
+	
+    public void volumenDown () {
+	if ((this.estado == true) && (this.volumen > 1) ) {
+		this.volumen--;
+	}
     }
 }  
 
